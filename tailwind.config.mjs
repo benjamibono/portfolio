0ea63989d6767f8287eee77a9c421e5462bf8080
benjamibono <benjamibono@gmail.com>
2025-01-10 19:19:02 +0100

    mid commit message

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+        extend: {
+            colors: {
+                primary: 'var(--primary-text-color)',
+                secondary: 'var(--secondary-text-color)',
+                muted: 'var(--muted-text-color)',
+                accent: 'var(--accent-text-color)',
+                background: 'var(--background-color)',
+            },
+        },
+    },
+    plugins: [],
 }
+
